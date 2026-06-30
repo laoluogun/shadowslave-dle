9a968e8598f045d144f7d0e0c8aa6d0eff447eb5
laolu <femiogun9590@gmail.com>
2026-06-30 12:25:32 -0400

    Add images connected to characters.

---
+++ src/data/characters.js
@@ -0,0 +1,10 @@
+export const characters = {
+  Sunless: { image: "/images/sunless.png" },
+  Nephis: { image: "/images/nephis.png" },
+  Cassie: { image: "/images/cassie.png" },
+  Jet: { image: "/images/jet.png" },
+  Kai: { image: "/images/kai.png" },
+  Effie: { image: "/images/effie.png" },
+  Mordret: { image: "/images/mordret.png" },
+  Rain: { image: "/images/rain.png" },
+}
