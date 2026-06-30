191e9e1ac8286342f8d75c239d6178a1df52b0e0
laolu <femiogun9590@gmail.com>
2026-06-30 11:44:17 -0400

    Create quote file and store quotes for select characters.

---
+++ src/data/quotes.js
@@ -0,0 +1,120 @@
+export const quotes = [
+  {
+    id: 1,
+    quote: "All the gods and all the demons in the world won't be able to make me leave you again. So don't you leave me behind, either.",
+    speaker: "Sunless",
+    recipient: "Nephis",
+    chapter: "Chapter 2992 Never Let Me Go"
+  },
+  
+  {
+    id: 2,
+    quote: "Suffering from mental illnesses? How trite. If anything, mental illnesses suffer from me...",
+    speaker: "Sunless",
+    recipient: "Kai",
+    chapter: "Chapter 2318 A New Paradigm"
+  },
+
+  {
+    id: 3,
+    quote: "Trust me. I'm the most honest person in the world. Two worlds, in fact.",
+    speaker: "Sunless",
+    recipient: "Cassie",
+    chapter: "Chapter 55 Lucky People"
+  },
+
+  {
+    id: 4,
+    quote: "Why should I settle for submission when I can have loyalty? People who will follow me, serve me, and obey me will do so because that is their most ardent desire.",
+    speaker: "Nephis", 
+    recipient: "Sunless",
+    chapter: "Chapter 808 A Long-Awaited Conversation"
+  },
+  {
+    id: 5,
+    quote: "I am tired of compromises. I am tired of being reasonable. I am tired of being restrained.",
+    speaker: "Nephis",
+    recipient: "Soul Stealer",
+    chapter: "Chapter 1517 Supernova"
+  },
+  {
+    id: 6,
+    quote: "But… I hope that you make a different choice. That you will stay with us, despite everything.",
+    speaker: "Cassie",
+    recipient: "Sunless",
+    chapter: "Chapter 1560 Freedom of Choice" 
+  },
+  {
+    id: 7,
+    quote: "To be honest, I am a bit devious. And l am a bit eerie. Treacherous... that too, I think. I just hide it better than most — maybe better than anyone. But I am really trying my best.",
+    speaker: "Cassie",
+    recipient: "The Other Mordret",
+    chapter: "Chapter 2903 To See, to Know, to Remember"
+  },
+  {
+    id: 8,
+    quote: "It takes a certain kind of person to face the very depths of despair, and still continue to fight.",
+    speaker: "Jet",
+    recipient: "Sunless",
+    chapter: "Chapter 812 Naval Convoy"
+  },
+  {
+    id: 9,
+    quote: "Welcome to the club… the I Killed Myself club?",
+    speaker: "Jet",
+    recipient: "Kai",
+    chapter: "Chapter 1526 Tempered in Flame"
+  },
+  {
+    id: 10,
+    quote: "I have never not been in a battle. I am a descendant of the Goddess of Life, after all. And life is war.",
+    speaker: "Mordret",
+    recipient: "Cassie",
+    chapter: "Chapter 2902 Enemy at the Gate"
+  },
+  {
+    id: 11,
+    quote: "Ah, you know. The weather is not great, but the people are really… charming. Well, you've met Seishan and Beastmaster, so you must have an idea. It's like I'm living in a flower garden.",
+    speaker: "Mordret",
+    recipient: "Sunless",
+    chapter: "Chapter 1527 Leaving Twilight"
+  },
+  {
+    id: 12,
+    quote: "Everyone in this dark city is convinced that this is hell. But to me, it's a paradise. You wouldn't understand. But for some of us, the real world was more of a hell than the Dream Realm.",
+    speaker: "Effie",
+    recipient: "Sunless",
+    chapter: "Chapter 188 Of Heaven And Hell"
+  },
+  {
+    id: 13,
+    quote: "Listen to Sunny, kid. He might look like a wet weasel, but this guy knows his stuff. The things he lived through are simply beyond reason. Honestly, if he had any shame, he would be dead.",
+    speaker: "Effie",
+    recipient: "Rain",
+    chapter: "Chapter 501 Step Two"
+  },
+  {
+    id: 14,
+    quote: "Let's just say that, if I had the choice, I would have preferred to stay blissfully blind to the truth forever.",
+    speaker: "Kai",
+    recipient: "Sunless",
+    chapter: "Chapter 211 Game of Lies"
+  },
+  {
+    id: 15,
+    quote: "…Just out of curiosity, how handsome are we talking? What about his style? Who makes his clothes?",
+    speaker: "Kai",
+    recipient: "Effie",
+    chapter: "Chapter 1720 Hard Day at Work"
+  },
+  {
+    id: 16,
+    quote: "Sure. After all, as a Legacy, I should receive some cool stuff, shouldn't I? I swear, there has never been a poorer Legacy than me… and a stingier founder of a Legacy Clan than you.",
+    speaker: "Rain",
+    recipient: "Sunless",
+    chapter: "Chapter 1977 Their Legacy"
+  }
+
+
+
+]
