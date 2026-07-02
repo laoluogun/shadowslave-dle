9638faacaa7522f8be980b410445c8670079188e
laolu <femiogun9590@gmail.com>
2026-07-02 15:24:25 -0400

    Abstract GuessHistory
    For readability and resuability, package guessHistory as its own separate file.

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import VolumeClue from './VolumeClue'
 import ClueButtons from './ClueButtons'
 import InputField from './InputField'
 import Suggestions from './Suggestions'
+import GuessHistory from './GuessHistory'
 
 function App() {
 
@@ -148,25 +149,8 @@ function App() {
               {feedback}
             </p>
           )}
-        <ol className="mt-4 space-y-2 ">
           {/* Display the history of guesses in reverse order, with styling based on correctness */}
-          {[...guessHistory].reverse().map((pastGuess, index) => (
-                <li
-                  key={index}
-                  className={`px-3 py-2 border rounded-lg flex flex-col items-center justify-between text-white
-                    ${pastGuess === speaker 
-                      ? 'bg-green-800/80 border-green-600' 
-                      : 'bg-red-900/80 border-red-700'}`}
-                >  
-                <div className="flex gap-2 ">
-                  <img src={characters[pastGuess]?.image} 
-                  alt={pastGuess} 
-                  className="w-14 h-14 object-cover rounded-xs" />
-                </div>
-              <span>{pastGuess}</span>
-            </li>
-          ))}
-        </ol>
+        <GuessHistory guessHistory={guessHistory} speaker={speaker} />
       </div>
     </div>
     </>
---
+++ src/GuessHistory.jsx
@@ -0,0 +1,27 @@
+import {characters} from './data/characters'
+
+function GuessHistory({ guessHistory, speaker }) {
+    return (
+      <ol className="mt-4 space-y-2 ">
+                {/* Display the history of guesses in reverse order, with styling based on correctness */}
+                {[...guessHistory].reverse().map((pastGuess, index) => (
+                      <li
+                        key={index}
+                        className={`px-3 py-2 border rounded-lg flex flex-col items-center justify-between text-white
+                          ${pastGuess === speaker 
+                            ? 'bg-green-800/80 border-green-600' 
+                            : 'bg-red-900/80 border-red-700'}`}
+                      >  
+                      <div className="flex gap-2 ">
+                        <img src={characters[pastGuess]?.image} 
+                        alt={pastGuess} 
+                        className="w-14 h-14 object-cover rounded-xs" />
+                      </div>
+                    <span>{pastGuess}</span>
+                  </li>
+                ))}
+              </ol>
+            )
+        }
+
+export default GuessHistory
