8357affc9753a51e4a5eab356a9a267d871b3729
laolu <femiogun9590@gmail.com>
2026-07-04 15:24:18 -0400

    Brighten border radius of InputField

--- src/InputField.jsx
+++ src/InputField.jsx
@@ -3,7 +3,7 @@ function InputField({guess, handleChange}) {
     <input 
                   value={guess} 
                   onChange={handleChange}
-                  className="w-full bg-zinc-800/80 border border-zinc-600 rounded-none px-3 py-2 text-white placeholder:text-zinc-400 focus:outline-none focus:border-zinc-400 transition"
+                  className="w-full bg-zinc-800/80 border border-zinc-500 rounded-none px-3 py-2 text-white placeholder:text-zinc-400 focus:outline-none focus:border-zinc-400 transition"
                   placeholder="Type your guess here..."
                 />
     )
