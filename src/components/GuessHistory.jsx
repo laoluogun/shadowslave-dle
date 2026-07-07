cbba324847a5e4bdfbdf17a58f5d7898b3ceb93e
laolu <femiogun9590@gmail.com>
2026-07-07 12:39:51 -0400

    Darken colors in GuessHistory containers

--- src/components/GuessHistory.jsx
+++ src/components/GuessHistory.jsx
@@ -1,23 +1,23 @@
-import {characters} from '../data/characters'
+import {quoteCharacters} from '../data/quoteCharacters'
 
 function GuessHistory({ guessHistory, speaker }) {
     return (
-      <ol className="mt-4 space-y-2 ">
+      <ol className="mt-4 space-y-2">
                 {/* Display the history of guesses in reverse order, with styling based on correctness */}
                 {[...guessHistory].reverse().map((pastGuess, index) => (
                       <li
                         key={index}
                         className={`px-3 py-2 border rounded-none flex flex-col items-center justify-between text-white
                           ${pastGuess === speaker 
-                            ? 'bg-green-800/80 border-green-600' 
-                            : 'bg-red-900/80 border-red-700'}`}
+                            ? 'bg-green-950/90 border-green-800' 
+                            : 'bg-red-950/90 border-red-800'}`}
                       >  
                       <div className="flex gap-2 ">
-                        <img src={characters[pastGuess]?.image} 
+                        <img src={quoteCharacters[pastGuess]?.image} 
                         alt={pastGuess} 
-                        className="w-14 h-14 object-cover rounded-none" />
+                        className="w-20 h-20 object-cover rounded-none border border-zinc-600" />
                       </div>
-                    <span>{pastGuess}</span>
+                    <span className='text-md'>{pastGuess}</span>
                   </li>
                 ))}
               </ol>
