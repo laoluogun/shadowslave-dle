a5bd621189488969ff5074fde7b588dec868e4b5
laolu <femiogun9590@gmail.com>
2026-07-07 12:38:48 -0400

    Add border to recipient clue image

--- src/components/RecipientClue.jsx
+++ src/components/RecipientClue.jsx
@@ -2,7 +2,7 @@ function RecipientClue({recipientName, recipientImage}) {
     return (
                 <div className="bg-zinc-800/80 border border-zinc-600 rounded-none p-4 mt-3 text-center text-white">
                   <p className="font-semibold text-lg">{recipientName}</p>
-                  <img src={recipientImage} alt={recipientName} className="w-20 h-20 object-cover rounded-none mx-auto mt-2"/>
+                  <img src={recipientImage} alt={recipientName} className="w-20 h-20 object-cover rounded-none border border-zinc-600 mx-auto mt-2"/>
                 </div>
               )
 }
