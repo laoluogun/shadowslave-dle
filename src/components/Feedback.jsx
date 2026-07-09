19d47b67d966b6b24e2de4ae75b5a978739a26a2
laolu <femiogun9590@gmail.com>
2026-07-09 10:36:54 -0400

    Remove red text from feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,7 +1,7 @@
 function Feedback({feedback}) {
     return (
         <p className={`text-center font-semibold ${
-              feedback.includes('Correct') ? 'text-green-700' : 'text-red-700'
+              feedback.includes('Correct') ? 'text-green-700' : 'text-zinc-100'
             }`}>
               {feedback}
             </p>
