c9aa876602d3efca486a031cc0603b13d7ed4487
laolu <femiogun9590@gmail.com>
2026-07-13 11:50:49 -0400

    Adjust clue cards to match color scheme of matching clue buttons

--- src/components/RecipientClue.jsx
+++ src/components/RecipientClue.jsx
@@ -1,6 +1,6 @@
 function RecipientClue({recipientName, recipientImage}) {
     return (
-                <div className="bg-zinc-800/80 border border-zinc-600 rounded-none p-4 mt-3 text-center text-white">
+                <div className="bg-zinc-700/80 border border-zinc-400 rounded-none p-4 mt-3 text-center text-white">
                   <p className="font-semibold text-lg">{recipientName}</p>
                   <img src={recipientImage} alt={recipientName} className="w-20 h-25 object-cover rounded-none border border-zinc-600 mx-auto mt-2"/>
                 </div>
--- src/components/VolumeClue.jsx
+++ src/components/VolumeClue.jsx
@@ -1,6 +1,6 @@
 function VolumeClue({volume, chapter}) {
     return(
-         <div className="bg-zinc-800/80 border border-zinc-600 rounded-none p-4 mt-3 text-center text-white">
+         <div className="bg-zinc-700/80 border border-zinc-400 rounded-none p-4 mt-3 text-center text-white">
               <p className="text-zinc-300">{volume} - {chapter}</p>
          </div>
     )
