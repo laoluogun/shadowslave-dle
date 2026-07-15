d4b10fe107c43974b1c85cce555303cfc24299bd
laolu <femiogun9590@gmail.com>
2026-07-15 11:47:21 -0400

    Create classicHelp component stating necessary information to play

---
+++ src/components/classicHelp.jsx
@@ -0,0 +1,22 @@
+function classicHelp() {
+    return (
+    <div className="flex flex-col gap-1">
+            <h1 className="underline font-mountain-king">Shadow Slave-Dle Classic Guide</h1>
+            <p className="text-xs">In classic mode, type in the name of a character and it will reveal clues assosciated with today's character.
+            The color of the tiles will change to show how close your guess was to the character to find.</p>
+            <p className="text-xs">Green tiles indicate an exact match.</p>
+            <p className="text-xs">Yellow tiles indicate a partial match, meaning some information within the tile matches with today's character.</p>
+            <p className="text-xs">Red tiles indicate that there are no shared properties.</p>
+            <h2 className="underline font-mountain-king">Properties</h2>
+            <ol className="text-xs gap-1">
+                <li>Gender - Male, Female, None, Unknown</li>
+                <li>Race - Human, Nightmare Creature, True Defiled, Daemon, Shadow</li>
+                <li>Affiliations - Affiliations associated with a character</li>
+                <li>Rank - The highest rank the character has achieved</li>
+                <li>True Name - Whether the character possesses a true name</li>
+                <li>Debut - When a character first appears/is mentioned within the story. More info on the information popup. </li>
+            </ol>
+     </div>
+)}
+
+export default classicHelp
