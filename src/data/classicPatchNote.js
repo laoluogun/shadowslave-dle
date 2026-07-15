c090d8e36364201c9c7a4b419ccd393a71e2c9ce
laolu <femiogun9590@gmail.com>
2026-07-15 12:37:37 -0400

    Create template patchnote information for both modes

---
+++ src/data/classicPatchNote.js
@@ -0,0 +1,3 @@
+const patchnotetext = 'There are no current patchnotes for the classic mode! This project is currently in development.'
+
+export default patchnotetext
---
+++ src/data/quotePatchNote.js
@@ -0,0 +1,3 @@
+const patchnotetext = 'There are no current patchnotes for the quote mode! This project is currently in development.'
+
+export default patchnotetext
