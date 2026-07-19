c979bd8c55c42e8756f830c6e2542c322cc1ea55
laolu <femiogun9590@gmail.com>
2026-07-19 12:40:23 -0400

    Edit help tabs design
    Adjust the size and stylistic choices in the classic and quote help components

--- src/components/QuoteHelp.jsx
+++ src/components/QuoteHelp.jsx
@@ -1,11 +1,11 @@
 function classicHelp() {
     return (
     <div className="flex flex-col gap-1">
-            <h1 className="underline font-mountain-king">Shadow Slave-Dle quote Guide</h1>
-            <p className="text-xs">In quote mode, your goal is to find out who is the speaker of the quote. </p>
-            <p className="text-xs">After a certain number of guesses, clues will become available to you to help narrow down your choices.</p>
-            <h2 className="underline font-mountain-king">Clues</h2>
-            <ol className="text-xs gap-1">
+            <h1 className="underline font-semibold">Shadow Slave-Dle Quote Guide</h1>
+            <p className="text-sm">In quote mode, your goal is to find out who is the speaker of the quote. </p>
+            <p className="text-sm">After a certain number of guesses, clues will become available to you to help narrow down your choices.</p>
+            <h2 className="underline font-semibold">Clues</h2>
+            <ol className="text-sm gap-1">
                 <li>Recipient Clue - This is the character to whom the speaker spoke to.</li>
                 <li>Chapter Clue - The exact volume and chapter the quote was uttered.</li>
             </ol>
--- src/components/classicHelp.jsx
+++ src/components/classicHelp.jsx
@@ -1,15 +1,15 @@
 function classicHelp() {
     return (
     <div className="flex flex-col gap-1">
-            <h1 className="underline font-mountain-king">Shadow Slave-Dle Classic Guide</h1>
-            <p className="text-xs">In classic mode, type in the name of a character and it will reveal clues assosciated with today's character.
+            <h1 className="underline font-semibold">Shadow Slave-Dle Classic Guide</h1>
+            <p className="text-sm">In classic mode, type in the name of a character and it will reveal clues assosciated with today's character.
             The color of the tiles will change to show how close your guess was to the character to find.</p>
-            <p className="text-xs"><span className="text-green-400">Green</span> tiles indicate an exact match.</p>
-            <p className="text-xs"><span className="text-yellow-400">Yellow</span> tiles indicate a partial match, meaning some information within the tile matches with today's character.</p>
-            <p className="text-xs"><span className="text-red-400">Red</span> tiles indicate that there are no shared properties.</p>
-            <p className="text-xs"><span className="text-red-400">Red</span> tiles with arrows indicate that the correct property is either of a higher or lower value.</p>
-            <h2 className="underline font-mountain-king">Properties</h2>
-            <ol className="text-xs list-disc gap-1 pl-3">
+            <p className="text-sm"><span className="text-green-400">Green</span> tiles indicate an exact match.</p>
+            <p className="text-sm"><span className="text-yellow-400">Yellow</span> tiles indicate a partial match, meaning some information within the tile matches with today's character.</p>
+            <p className="text-sm"><span className="text-red-400">Red</span> tiles indicate that there are no shared properties.</p>
+            <p className="text-sm"><span className="text-red-400">Red</span> tiles with arrows indicate that the correct property is either of a higher or lower value.</p>
+            <h2 className="underline font-semibold">Properties</h2>
+            <ol className="text-sm list-disc gap-1 pl-3">
                 <li>Gender - Male, Female, None, Unknown</li>
                 <li>Race - Human, Nightmare Creature, True Defiled, Daemon, Shadow</li>
                 <li>Affiliations - Affiliations associated with a character</li>
