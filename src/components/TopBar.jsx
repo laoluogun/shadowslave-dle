bc9f1f5c900895fb977892279f993238bbfb1cc1
laolu <femiogun9590@gmail.com>
2026-07-20 17:36:09 -0400

    Largen top bar buttons when hovered

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -20,7 +20,7 @@ function TopBar({ statsContent, currentStreak, patchContent, helpContent }) {
           className={`p-2 rounded-none transition
             ${button.noModal 
               ? 'border-transparent cursor-default' 
-              : 'hover:bg-zinc-600 cursor-pointer'}`}
+              : 'hover:bg-zinc-600 cursor-pointer hover:scale-105 duration-150'}`}
         >
           {button.id === 'streak' ? (
             <div className="relative inline-flex items-center justify-center w-8 h-8">
