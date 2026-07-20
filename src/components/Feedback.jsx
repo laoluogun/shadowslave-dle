236ebed8417d801cf6455e75913de060fccc94ab
laolu <femiogun9590@gmail.com>
2026-07-20 18:09:51 -0400

    Add background to try improve readability

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,10 +1,12 @@
 function Feedback({feedback}) {
     return (
-        <p className={`text-center font-semibold ${
-              feedback.includes('Correct') ? 'text-green-700' : 'text-zinc-100'
-            }`}>
-              {feedback}
+        <div className="flex w-xs self-center bg-zinc-700/70 justify-center items-center border border-zinc-950 py-1">
+            <p className={`text-center font-semibold ${
+                  feedback.includes('Correct') ? 'text-green-300' : 'text-zinc-100'
+                }`}>
+                  {feedback}
             </p>
+        </div>
         )
     }
 
