c35da8ce602584b18aef32649362d76f82797279
laolu <femiogun9590@gmail.com>
2026-07-22 15:04:45 -0400

    Remove trailing space at the end of flaw/quote

--- src/components/FlawCard.jsx
+++ src/components/FlawCard.jsx
@@ -2,7 +2,7 @@ function FlawCard({ flaw }) {
   return (
     <div className="bg-transparent rounded-none shadow-md p-6 mb-6 text-center">
         <h2 className="text-md uppercase text-zinc-300 mb-2">Which character has a flaw with this description?</h2>
-        <p className="text-lg text-white italic">❝ {flaw} ❞</p>
+        <p className="text-lg text-white italic">❝ {flaw}❞</p>
     </div>
   )
 }
--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -2,7 +2,7 @@ function QuoteCard({ quote }) {
   return (
     <div className="bg-transparent rounded-none shadow-md p-6 mb-6 text-center">
         <h2 className="text-md uppercase text-zinc-300 mb-2">Which character said this?</h2>
-        <p className="text-lg text-white italic">❝ {quote} ❞</p>
+        <p className="text-lg text-white italic">❝ {quote}❞</p>
     </div>
   )
 }
