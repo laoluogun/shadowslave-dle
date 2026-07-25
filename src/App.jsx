eda7ffc39df3a7027e7976c37b49b20b5273a70c
laolu <femiogun9590@gmail.com>
2026-07-25 12:29:02 -0400

    Preload UI Icons

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,37 @@ import QuotePage from './pages/QuotePage'
 import ClassicPage from './pages/ClassicPage'
 import FlawsPage from './pages/FlawsPage'
 import ArtistCredits from './components/ArtistCredits'
+import fireIcon from './assets/images/fire.png'
+import notesIcon from './assets/images/notes.png'
+import questionIcon from './assets/images/question.png'
+import questionSignIcon from './assets/images/question-sign.png'
+import quoteIcon from './assets/images/quote.png'
+import brokenPlateIcon from './assets/images/broken-plate.png'
+import infoIcon from './assets/images/info.png'
+import talkIcon from './assets/images/talk.png'
+import bookIcon from './assets/images/book.png'
 
+const UI_ICONS = [
+  statsIcon, fireIcon, notesIcon, questionIcon,
+  questionSignIcon, quoteIcon, brokenPlateIcon,
+  infoIcon, talkIcon, bookIcon
+]
 
 function App() {
 
-  //Preload images for all characters to ensure they display quickly when needed
+
   useEffect(() => {
+    //Preload images for all characters to ensure they display quickly when needed
     Object.values(characters).forEach(character => {
       const img = new Image()
       img.src = character.image
     })
+
+     // Preload UI icons
+      UI_ICONS.forEach(src => {
+        const img = new Image()
+        img.src = src
+      })
   }, [])
 
 
