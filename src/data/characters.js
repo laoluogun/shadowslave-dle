a481e9faca47ef6191c1700d3f459a8e946199cc
laolu <femiogun9590@gmail.com>
2026-07-26 10:08:42 -0400

    Update Jet

--- src/data/characters.js
+++ src/data/characters.js
@@ -64,7 +64,7 @@ export const characters = {
         race: 'Human',
         affiliations: ['Government', 'First Irregular Company', 'Nephis\' Cohort'],
         currentRank: "Transcendent",
-        hasTrueName: true,
+        hasTrueName: false,
         debutVolume: "Volume 1: Child of Shadows"
     },
 
