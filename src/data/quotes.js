3a1f1cf56bb21b6a5d3316189f2b594f7c8ccce1
laolu <femiogun9590@gmail.com>
2026-07-31 11:04:15 -0400

    Add quotes to new characters

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -50,15 +50,14 @@ export const quotes = [
     chapter: "Chapter 1560 Freedom of Choice" ,
     volume: "Volume 7: The Tomb of Ariel"
   },
-  /** Other Mordret does not have an image therefore does not work
   {
     id: 7,
-    quote: "To be honest, I am a bit devious. And l am a bit eerie. Treacherous... that too, I think. I just hide it better than most — maybe better than anyone. But I am really trying my best.",
+    quote: "This island is our home! It's so nice here, with the three of us together! Why do you want to leave?!,",
     speaker: "Cassie",
-    recipient: "The Other Mordret",
-    chapter: "Chapter 2903 To See, to Know, to Remember",
-    volume: "Volume 11: The Song of Ariadne"
-  },*/
+    recipient: "Sunless",
+    chapter: "Chapter 90 Nightfall",
+    volume: "Volume 1: Child of Shadows"
+  },
   {
     id: 8,
     quote: "It takes a certain kind of person to face the very depths of despair, and still continue to fight.",
@@ -211,8 +210,365 @@ export const quotes = [
     recipient: "Nephis",
     chapter: "Chapter 2757 The Three of Us",
     volume: "Volume 11: The Song of Ariadne"
-  }
+  }, 
+  {
+    id: 26,
+    quote: "Maybe not... but over the years, wearing this blindfold became a habit. I like it. Every time I put it on, I feel like I am putting on a helmet and lowering its visor. And then, I am ready for battle.",
+    speaker: "Cassie",
+    recipient: "Mordret",
+    chapter: "Chapter 2902 Enemy at the Gate",
+    volume: "Volume 11: The Song of Ariadne"
+  },
+  {
+    id: 27,
+    quote: "If our tale has been forgotten, then let it remain so. It's not something to be proud of, anyway... and all of us are dead now, hopefully.",
+    speaker: "Eurys",
+    recipient: "Sunless",
+    chapter: "Chapter 2123 Courting Death",
+    volume: "Volume 9: Throne of War"
+  },
+  {
+    id: 28,
+    quote: "Hey, hey! My jokes are top-notch! Why are you threatening her with something so wonderful?",
+    speaker: "Jest",
+    recipient: "Cassie",
+    chapter: "Chapter 2140 Terrible Offer",
+    volume: "Volume 9: Throne of War"
+  },
+  {
+    id: 29,
+    quote: "You will accomplish even more, given time. What, do you think I was already brilliant at your age? Gods, no! I barely knew how to tie my shoes. Letting someone young take my place is a privilege of mine…",
+    speaker: "Obel",
+    recipient: "Beth",
+    chapter: "Chapter 1054 The Fall of Falcon Scott (72)",
+    volume: "Volume 5: Dread Night"
+  },
+  {
+    id: 30,
+    quote: "When I was young, many people were convinced that we were all going to go extinct soon. But we didn't. Then, they went around saying that if things did not change for the better, humanity would surely be doomed. Things changed for the worst, but humanity still persisted. Then, the Nightmare Spell appeared, and it seemed like all their fears came true... and yet, here we are. Still kicking.",
+    speaker: "Obel",
+    recipient: "Sunless",
+    chapter: "Chapter 911 Question of Value",
+    volume: "Volume 5: Dread Night"
+  },
 
+  {
+    id: 31,
+    quote: "I lived a long life... far too long, really. Gods, I am older than my grandmother was when she passed away - by a few centuries, at least. Ah, these old bones of mine have had enough.",
+    speaker: "Wind Flower",
+    recipient: "Sunless",
+    chapter: "Chapter 1469 Something to Remember",
+    volume: "Volume 7: Tomb of Ariel"
+  },
+  {
+    id: 32,
+    quote: "Be brave boy. The gods are watching.",
+    speaker: "Solvane",
+    recipient: "Elyas",
+    chapter: "Chapter 624 A New Day",
+    volume: "Volume 4: Chain Breaker"
+  }, 
+
+  {
+    id: 33,
+    quote: "This is our chance. This is what we've been fighting for all this time, is it not? The tales my mother told me were all true... I'll free us both! The Lord of Light will guide my hand...",
+    speaker: "Elyas",
+    recipient: "Sunless",
+    chapter: "Chapter 624 A New Day",
+    volume: "Volume 4: Chain Breaker"
+  }, 
+
+  {
+    id: 34,
+    quote: "...Of course. Why did I expect different from the servent of cowardly Shadow.",
+    speaker: "Solvane",
+    recipient: "Sunless",
+    chapter: "Chapter 624 A New Day",
+    volume: "Volume 4: Chain Breaker"
+  }, 
+
+  {
+    id: 35,
+    quote: "Do you even remember your own name, pathetic thing?",
+    speaker: "Weaver",
+    recipient: "Orphne",
+    chapter: "Chapter 2410 Myriad of Stars",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+
+  {
+    id: 36,
+    quote: "Are you there? Are you watching? Watch well then, epigone. Let me show you, how gods die.",
+    speaker: "Weaver",
+    recipient: "Sunless",
+    chapter: "Chapter 2410 Myriad of Stars",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+
+  {
+    id: 37,
+    quote: "You. I killed you.",
+    speaker: "Orphne",
+    recipient: "Weaver",
+    chapter: "Chapter 2410 Myriad of Stars",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+
+  {
+    id: 38,
+    quote: "What is your true name then?",
+    speaker: "Harper",
+    recipient: "Sunless",
+    chapter: "Chapter 161 Rubicon",
+    volume: "Volume 2: Demon of Change"
+  },
+
+  {
+    id: 39,
+    quote: "...I faield again. But I won't fail anymore.",
+    speaker: "Anvil",
+    recipient: "Jest",
+    chapter: "Chapter 2173 Paper Armor",
+    volume: "Volume 9: Throne of War"
+  },
+
+  {
+    id: 40,
+    quote: "Oh, this is really fun! But a bit tiring. How about I just cut off your pretty head right now and end this?",
+    speaker: "Gunlaug",
+    recipient: "Nephis",
+    chapter: "Chapter 296 Creature of Light",
+    volume: "Volume 2: Demon of Change"
+  },
+
+  {
+    id: 41,
+    quote: "I'm just like our mother, in the end... I can't, I can't... I can't kill them...",
+    speaker: "Seishan",
+    recipient: "Beastmaster",
+    chapter: "Chapter 2818 Blood Bond",
+    volume: "Volume 11: Song of Ariadne"
+  },
+
+  {
+    id: 42,
+    quote: "Naturally, it was like a treasury for us when we were children. But... some of those toys were really dangerous.",
+    speaker: "Revel",
+    recipient: "Sunless",
+    chapter: "Chapter 2306 Forbidden Fruit",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+
+   {
+    id: 43,
+    quote: "...should we kill that girl, Changing Star?",
+    speaker: "Beastmaster",
+    recipient: "Seishan",
+    chapter: "Chapter 1125 Private Conversations",
+    volume: "Volume 6: All The Devils Are Here"
+  },
+
+  {
+    id: 44,
+    quote: "Sure. Asterion will fit just fine.",
+    speaker: "Tamar",
+    recipient: "Rain",
+    chapter: "Chapter 2424: The Word is Mightier than a Grenade",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+
+  {
+    id: 45,
+    quote: "...Abomination.",
+    speaker: "Samara",
+    recipient:  "Sunless",
+    chapter: "Chapter 921 New Pets",
+    volume: "Volume 5: Dread Night"
+  },
+
+  {
+    id: 46, 
+    quote: "You dare peer into my mind? Brave man! Oh, but I wouldn't advise it... I wouldn't advise it at all...",
+    speaker: "Mad Prince",
+    recipient: "Asterion",
+    chapter: "Chapter 2986 A Memory Most Dreadful",
+    volume: "Volume 11: Song of Ariadne"
+  },
+
+  {
+    id: 47,
+    quote: "I used to find their corpses drifting in the water. No wounds, no marks left on them. They were just dead, as if their very life had been stolen from them.",
+    speaker: "Ananke",
+    recipient: "Sunless",
+    chapter: "Chapter 2939 Far From The Tree",
+    volume: "Volume 11: Song of Ariadne"
+  },
+  {
+    id: 48,
+    quote: "It is indeed a family affair. Our family, which you slaughtered!",
+    speaker: "Naeve",
+    recipient: "Mordret",
+    chapter: "Chapter 1912 Self-Reflection",
+    volume: "Volume 9: Throne of War"
+  },
+  {
+    id: 49,
+    quote: "I agree. It is too perilous, even if the rewards promised by that expedition are great.",
+    speaker: "Aether",
+    recipient: "Sunless",
+    chapter: "Chapter 2604 Mindset of a Sovereign",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+  {
+    id: 50,
+    quote: "I really can't remember. Damnation! My memory has gone bad.",
+    speaker: "Ling",
+    recipient:  "Aiko",
+    chapter: "Chapter 2434 The Terrible Price of Saving the World",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+  {
+    id: 51, 
+    quote: "Hey yourself, Gargantuanne. Look, I brought your Pantagruel.",
+    speaker: "Aiko",
+    recipient: "Effie",
+    chapter:  "Chapter 2428 Fairy God Mother",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+  {
+    id: 52, 
+    quote: "Exactly! The lesson you were supposed to learn today is that having friends is more important than having money.",
+    speaker: "Aiko",
+    recipient: "Ling",
+    chapter:  "Chapter 2430 Aunti Aiko's Lessons",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+  
+  {
+    id: 53,
+    quote: "Don't you know where you are, abominable creature?! I am here because I led my armies into the great war, slaughtered a myriad of souls, and was punished for my strength and my pride!",
+    speaker: "Azarax",
+    recipient: "Nephis",
+    chapter: "Chapter 500 In the Depths of a Nightmare",
+    volume: "Volume 3: Prince of Nothing"
+  },
+  {
+    id: 54,
+    quote: "So... you do bleed, after all.",
+    speaker: "Beth",
+    recipient: "Sunless",
+    chapter: "Chapter 963 Supply Depot",
+    volume: "Volume 5: Dread Night"
+  },
+  {
+    id: 55,
+    quote: "Freedom, young man... freedom can be the most appalling curse in the world, if it falls into the wrong hands. It is a terrifying thing.",
+    speaker: "Cor",
+    recipient: "Sunless",
+    chapter: "Chapter 1098 Appaling Curse",
+    volume: "Volume 6: All The Devils Are Here"
+  },
+  {
+    id: 56,
+    quote: "Sometimes, I forget...How much I loathe you and your family.",
+    speaker: "Gilead",
+    recipient: "Morgan",
+    chapter: "Chapter 1151 See no Evil",
+    volume: "Volume 6: All The Devils Are Here"
+  },
+  {
+    id: 57,
+    quote: "Poor worm. You don't even understand what loyalty means, do you? Alive or dead… it doesn't matter.",
+    speaker: "Harus",
+    recipient: "Sunless",
+    chapter: "Chapter 299 Twisted Reflection",
+    volume: "Volume 2: Demon of Change"
+  },
+  {
+    id: 58, 
+    quote: "Wonderful! This is wonderful! You're such an astute young man! Don't you worry. In four whole weeks, I will make you immortal…",
+    speaker: "Julius",
+    recipient: "Sunless",
+    chapter: "Chapter 25 Wilderness Survival",
+    volume: "Volume 1: Child of Shadows"
+  },
+  {
+    id:  59,
+    quote: "That demoness is the true horror... in terms of personal power, I alone am her equal.",
+    speaker: "Madoc",
+    recipient: "Nephis",
+    chapter: "Chapter 1102 Opposing Forces",
+    volume: "Volume 6: All The Devils Are Here"
+  },
+
+  {
+    id: 60,
+    quote: "It's just... refreshing. To be powerless, for once.",
+    speaker: "Nephis",
+    recipient: "Moonveil",
+    chapter: "Chapter 1892 Purity of Steel",
+    volume: "Volume 9: Throne of War"
+  },
+
+  {
+    id: 61,
+    quote: "Changing Star... what kind of monster are you?",
+    speaker: "Moonveil",
+    recipient: "Nephis",
+    chapter: "Chapter 1897 Above and Below",
+    volume: "Volume 9: Throne of War"
+  },
+
+  {
+    id: 62,
+    quote: "Ah the sun. I really did not think that I would ever see it again. One last time.",
+    speaker: "Nightwalker",
+    recipient: "Sunless",
+    chapter: "Chapter 2716 Grand Escape",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+
+  {
+    id: 63, 
+    quote: "Don't tell me… wait… are you here with a business proposal? Am I about to become a customer of Sunny's Brilliant Emporium?",
+    speaker: "Roan",
+    recipient: "Sunless",
+    chapter: "Chapter 402 Chain Worm",
+    volume: "Volume 3: Prince of Nothing"
+  },
+
+  {
+    id: 64,
+    quote: "Night, my dear friend. Are you sure this dirty vagrant is... a customer?",
+    speaker: "Stev",
+    recipient: "Kai",
+    chapter: "Chapter 172 Memory Market",
+    volume: "Volume 2: Demon of Change"
+  },
+  {
+    id: 65,
+    quote: "I wouldn't want a snake tattoo anywaay. Birds eat snakes.",
+    speaker: "Telle",
+    recipient: "Rain",
+    chapter: "Chapter 2291 An Elegant and Noble Outing",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+  {
+    id: 66, 
+    quote: "Well. IT's because... that woman. I am scared of her.",
+    speaker: "Bloodwave",
+    recipient: "Naeve",
+    chapter: "Chapter 2271 Bright Future",
+    volume: "Volume 10: Dark Lord's Dreadful Travelogue"
+  },
+  {
+    id: 67,
+    quote: "That reminds me of my solstice. The very first Nightmare Creature I stumbled upon managed to thoroughly crush both of my legs. I had to drag myself through the mud for two entire weeks of slow, painful crawling to get to the nearest Citadel.",
+    speaker: "Dorn",
+    recipient: "Samara",
+    chapter: "Chapter 829 Battle Scars",
+    volume: "Volume 5: Dread Night"
+  }
 
 
 ]
